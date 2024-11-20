6542f941001c5aa9d7821c07f75733742e4c97fb
Herilanto003 <herilantodenis@gmail.com>
2024-11-20 14:48:52 +0300

    premier commit

---
+++ assets/bootstrap.js
@@ -0,0 +1,11 @@
+// import { startStimulusApp } from '@symfony/stimulus-bundle';
+
+// const app = startStimulusApp();
+// // register any custom, 3rd party controllers here
+// // app.register('some_controller_name', SomeImportedController);
+
+
+import { Application } from '@hotwired/stimulus';
+
+// Initialiser Stimulus
+const application = Application.start();
