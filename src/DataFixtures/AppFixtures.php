67c4b6b558d28b11df1107f573674333f09664aa
Herilanto003 <herilantodenis@gmail.com>
2024-11-22 15:02:47 +0300

    Inscription et Connexion

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,15 +2,46 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\Students;
+use App\Entity\Teachers;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $level = ['M1', 'L3'];
+        $sexe = ['M', 'F'];
+        $grade = ['Docteur', 'Professeur', 'Docteur HDR'];
+
+        $faker = Factory::create('fr_FR');
+
+        // Generate 25 students
+        for ($i = 0; $i < 25; $i++) {
+            $student = new Students();
+
+            $student->setRegisterNumber($faker->numberBetween(200,300));
+            $student->setFirstname($faker->firstName());
+            $student->setLastname($faker->lastName());
+            $student->setLevel($level[array_rand($level)]);
+            $student->setSexe($sexe[array_rand($sexe)]);
+
+            $manager->persist($student);
+        }
+
+        // Generate 25 teachers
+        for ($i = 0; $i < 25; $i++) {
+            $teacher = new Teachers();
+
+            $teacher->setFirstname($faker->firstName());
+            $teacher->setLastname($faker->lastName());
+            $teacher->setSexe($sexe[array_rand($sexe)]);
+            $teacher->setGrade($grade[array_rand($grade)]);
+
+            $manager->persist($teacher);
+        }
 
         $manager->flush();
     }
